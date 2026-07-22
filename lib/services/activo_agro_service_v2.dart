3bbccffb31e796735a5e70b9b1c7baab773883fb
Javier Giacomin <giacojavier07@gmail.com>
2026-07-22 13:06:05 -0300

    style: eliminar import duplicado en ActivoAgroServiceV2

--- lib/services/activo_agro_service_v2.dart
+++ lib/services/activo_agro_service_v2.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../models/activos/activo_agro_model_v2.dart';
 import '../models/activos/confianza_activo_model.dart';
 
